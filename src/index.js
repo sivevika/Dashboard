ead3d49090ca0303ef1d696e48e4ef0aabca8822
Sive <sivevika@gmail.com>
2021-09-02 09:29:20 +0200

    Everything is normal

--- src/index.js
+++ src/index.js
@@ -15,6 +15,8 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
+
+
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
@@ -44,3 +46,5 @@ ReactDOM.render(
   </ThemeContextWrapper>,
   document.getElementById("root")
 );
+
+
